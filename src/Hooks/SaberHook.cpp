d97340578d352db9378d06e584b56c942f77a4c3
raineio <64136899+raineio@users.noreply.github.com>
2022-01-19 06:29:48 +0100

    Finished hook, need to test..

--- src/Hooks/SaberHook.cpp
+++ src/Hooks/SaberHook.cpp
@@ -8,13 +8,20 @@
 
 #include "beatsaber-hook/shared/utils/hooking.hpp"
 #include "include/Utilities/HookingUtility.hpp"
+#include "include/Utilities/ClawUtils.hpp"
+
+#include "UnityEngine/Transform.hpp"
 
 namespace Claws {
     MAKE_HOOK_MATCH(SaberModelContainer_Start, &GlobalNamespace::SaberModelContainer::Start, void, GlobalNamespace::SaberModelContainer* self) {
         SaberModelContainer_Start(self);
 
+        auto saber = self->dyn__saber();
+        auto saberTop = saber->dyn__saberBladeTopTransform();
+        auto saberBottom = saber->dyn__saberBladeBottomTransform();
 
-
+        saberTop->set_localPosition(UnityEngine::Vector3
+            (saberTop->get_localPosition().x, saberTop->get_localPosition().y, saberBottom->get_localPosition().z + 0.3));
     }
 }
 
