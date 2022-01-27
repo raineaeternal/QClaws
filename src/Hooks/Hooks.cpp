7d1c99ab83ee9d41f68679727047cc9199804f69
raineio <64136899+raineio@users.noreply.github.com>
2022-01-27 16:41:38 +0100

    ModelPrefab scaling

--- src/Hooks/Hooks.cpp
+++ src/Hooks/Hooks.cpp
@@ -8,21 +8,32 @@
 #include "include/Utilities/HookingUtility.hpp"
 #include "include/Utilities/ClawUtils.hpp"
 
+#include "include/main.hpp"
+
 #include "UnityEngine/Transform.hpp"
 
 MAKE_HOOK_MATCH(SaberModelContainer_Start, &GlobalNamespace::SaberModelContainer::Start, void, GlobalNamespace::SaberModelContainer* self) {
     SaberModelContainer_Start(self);
 
     auto saber = self->dyn__saber();
+    auto model = self->dyn__saberModelControllerPrefab();
+
     auto saberTop = saber->dyn__saberBladeTopTransform();
-    auto saberBottom = saber->dyn__saberBladeBottomTransform();
+    auto modelTop = model->get_transform();
 
-    auto pos = saberTop->get_localPosition();
-    pos.z += 0.3;
+    auto saberPos = saberTop->get_localPosition();
+    auto modelScale = modelTop->get_localScale();
 
-    saberTop->set_localPosition(pos);
+    saberPos.z = 0.3;
+    modelScale.z = 0.3;
+    modelScale.y = 0.3;
+    modelScale.x = 0.3;
+
+    saberTop->set_localPosition(saberPos);
+    modelTop->set_localScale(modelScale);
+
+    getLogger().info("length of model was: %.2f", modelScale.z);
 }
-
 
 void InstallClawHooks(Logger& logger) {
     INSTALL_HOOK(logger, SaberModelContainer_Start)
