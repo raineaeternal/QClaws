8d04132f5ba12cc07d1de9352e999c0c358a4e71
royston thorndycraft <18059207@brookes.ac.uk>
2022-03-19 12:56:12 +0000

    hopefully works

--- include/Utilities/ClawUtils.hpp
+++ include/Utilities/ClawUtils.hpp
@@ -16,13 +16,13 @@ namespace Claws::Utilities::ClawUtils {
         static inline std::map<Devices, UnityEngine::Vector3> DefaultTranslation = {
                 {Devices::Unknown, UnityEngine::Vector3()},
                 {Devices::Quest1,  UnityEngine::Vector3(-0.1, -0.0225, -0.06)},
-                {Devices::Quest2,  UnityEngine::Vector3(-0.05, -0.01, -0.1)}
+                {Devices::Quest2,  UnityEngine::Vector3(-0.005, 0.01, -0.1)}
         };
 
         static inline std::map<Devices, UnityEngine::Vector3> DefaultRotation = {
                 {Devices::Unknown, UnityEngine::Vector3()},
                 {Devices::Quest1,  UnityEngine::Vector3(25, 0, 90)},
-                {Devices::Quest2,  UnityEngine::Vector3(75, -5, 90)}
+                {Devices::Quest2,  UnityEngine::Vector3(25, -5, 90)}
         };
     };
 }
--- src/Hooks/Hooks.cpp
+++ src/Hooks/Hooks.cpp
@@ -4,6 +4,7 @@
 #include "GlobalNamespace/Saber.hpp"
 #include "GlobalNamespace/GamePause.hpp"
 #include "GlobalNamespace/SaberTypeObject.hpp"
+#include "GlobalNamespace/SaberTrail.hpp"
 
 #include "GlobalNamespace/OculusVRHelper.hpp"
 #include "GlobalNamespace/VRController.hpp"
@@ -17,62 +18,67 @@
 
 #include "UnityEngine/Transform.hpp"
 #include "UnityEngine/SceneManagement/Scene.hpp"
-
-MAKE_HOOK_MATCH(SaberModelContainer_Start, &GlobalNamespace::SaberModelContainer::Start,
-                void, GlobalNamespace::SaberModelContainer* self) {
-    SaberModelContainer_Start(self);
-
-    if (SaberModelContainer_Start == nullptr) return; {
-        if (UnityEngine::SceneManagement::Scene().get_name() == "GameCore") {
-            auto saber = self->dyn__saber();
-            auto model = self->GetComponent<GlobalNamespace::SaberModelController*>();
-
-            auto saberTop = saber->dyn__saberBladeBottomTransform();
-            auto modelTop = model->get_gameObject()->get_transform();
-
-            auto saberPos = saberTop->get_localPosition();
-            auto modelScale = modelTop->get_localScale();
-
-            saberPos.z = 0.3f;
-            modelScale.z = 0.3f;
-
-            saberTop->set_localPosition(saberPos);
-            modelTop->set_localScale(modelScale);
-        }
-    }
-}
+#include "UnityEngine/SceneManagement/SceneManager.hpp"
 
 using namespace GlobalNamespace;
 using namespace UnityEngine;
 using namespace Claws::Utilities;
+
+MAKE_HOOK_MATCH(SaberModelContainer_Start, &SaberModelContainer::Start, void, SaberModelContainer* self) {
+    SaberModelContainer_Start(self);
+    if (!getClawsConfig().Enabled.GetValue()) return;
+    if (SceneManagement::SceneManager::GetActiveScene().get_name() == "GameCore") {
+        getLogger().info("knob 3");
+        auto saber = self->dyn__saber();
+        auto model = self->get_transform()->GetComponentInChildren<SaberModelController*>(); // the previous line of this was causing a null deref lol
+
+        auto saberTop = saber->dyn__saberBladeBottomTransform();
+        auto saberIdk = model->dyn__saberTrail()->get_gameObject()->get_transform()->get_parent(); // dunno why this works but it moves both saber and trail
+
+        auto saberPos = saberTop->get_localPosition();
+        auto saberScale = saberIdk->get_localScale();
+
+        saberPos.z = 0.3f;
+        saberScale.z = 0.3f;
+        
+        saberIdk->set_localScale(saberScale);
+        saberTop->set_localPosition(saberPos);
+    }
+}
+
+Vector3 Mirror(Vector3 toMirror, bool isRot){
+    return isRot ? Vector3(toMirror.x, -1 * toMirror.y, -1 * toMirror.z) : Vector3(-1 * toMirror.x, toMirror.y, toMirror.z);
+
+}
+
 MAKE_HOOK_MATCH(ControllerTransform,
                 &OculusVRHelper::AdjustControllerTransform,
                 void,
                 OculusVRHelper* self,
-                UnityEngine::XR::XRNode node,
+                XR::XRNode node,
                 Transform* transform,
                 Vector3 position,
                 Vector3 rotation) {
-    if (!getClawsConfig().Enabled.GetValue()) {
-        return;
+    if (getClawsConfig().Enabled.GetValue()) {
+        if (node == XR::XRNode::RightHand){
+            if (transform->get_name() == "RightHand"){ //only applies to sabers while in level
+                position = position + ClawUtils::Preference::DefaultTranslation[Claws::DevicesExtensions::Headset()];
+                rotation = rotation + ClawUtils::Preference::DefaultRotation[Claws::DevicesExtensions::Headset()];
+            }
+        }
+        else if (node == XR::XRNode::LeftHand){
+            if (transform->get_name() == "LeftHand"){
+                position = position + Mirror(ClawUtils::Preference::DefaultTranslation[Claws::DevicesExtensions::Headset()], false);
+                rotation = rotation + Mirror(ClawUtils::Preference::DefaultRotation[Claws::DevicesExtensions::Headset()], true);
+            }
+        }
+        ControllerTransform(self, node, transform, position, rotation);
     }
-
-    if (node == XR::XRNode::RightHand) {
-        auto position = Claws::Utilities::ClawUtils::Preference::DefaultTranslation[Claws::DevicesExtensions::Headset()];
-        auto rotation = Claws::Utilities::ClawUtils::Preference::DefaultRotation[Claws::DevicesExtensions::Headset()];
-        self->AdjustControllerTransform(node, transform, position, rotation);
-    }
-
-    // if (node == XR::XRNode::LeftHand) {
-    //     position = Claws::Utilities::ClawUtils::Preference::DefaultTranslation[Claws::DevicesExtensions::Headset()];
-    //     rotation = Claws::Utilities::ClawUtils::Preference::DefaultRotation[Claws::DevicesExtensions::Headset()];
-    //     self->AdjustControllerTransform(node, transform, position, rotation);
-    // }
-    ControllerTransform(self, node, transform, position, rotation);
-}
+} 
 
 void InstallClawHooks(Logger& logger) {
     INSTALL_HOOK(logger, SaberModelContainer_Start)
+    INSTALL_HOOK(logger, ControllerTransform)
 }
 
 ClawsInstallHooks(InstallClawHooks);
